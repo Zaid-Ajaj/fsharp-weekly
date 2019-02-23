6f2be83b0602abd128fbbd765c1901827a6eaa7a
SergejDK <sedic001@stud.fh-dortmund.de>
2019-02-23 01:34:16 +0100

    android icons

--- FSharpWeekly.Android/Resources/Resource.designer.cs
+++ FSharpWeekly.Android/Resources/Resource.designer.cs
@@ -3944,13 +3944,19 @@ namespace FSharpWeekly.Android
 			public const int Appicon = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int icon = 2130903041;
+			public const int AppiconTransparent = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int icon_round = 2130903042;
+			public const int AppiconWhiteBackground = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int launcher_foreground = 2130903043;
+			public const int icon = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int icon_round = 2130903044;
+			
+			// aapt resource value: 0x7f030005
+			public const int launcher_foreground = 2130903045;
 			
 			static Mipmap()
 			{
